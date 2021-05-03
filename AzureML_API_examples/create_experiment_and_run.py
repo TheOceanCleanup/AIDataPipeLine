a1dcb6b1e7043b4c29809cff60eb8a74063ad4aa
Maarten van Berkel <maarten.vanberkel@theoceancleanup.com>
2021-05-03 21:48:53 +0200

    Added GPLv3 copyright notice.

--- AzureML_API_examples/create_experiment_and_run.py
+++ AzureML_API_examples/create_experiment_and_run.py
@@ -1,3 +1,19 @@
+# AIDAtaPipeLine - A series of examples and utilities for Azure Machine Learning Services
+# Copyright (C) 2020-2021 The Ocean Cleanup™
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 # Create a new experiment, environment (uncontrolled in this case) and use these
 # for a training run, using the 'images' dataset. It trains the model through
 # the training script train/train.py.
